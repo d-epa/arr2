8214141dafdbd4d562e0c15c370f50eee85f697c
deepa <deepa2362004@gmail.com>
2024-01-23 15:25:43 +0530

    array.java

---
+++ src/obj.java
@@ -0,0 +1,15 @@
+
+
+public class obj {
+    public static void main(String[] args) {
+        student s1= new student();
+        objects(s1);
+    }
+
+    public static void objects(student a)
+    {
+        System.out.println( a.name);
+        System.out.println(a.rollno);
+
+    }
+}
